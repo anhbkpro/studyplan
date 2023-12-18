262589f0ae55a70bee3d1a8ab3fa3008b46c9758
Anh Lai <laingocanh1990@gmail.com>
2023-12-18 16:45:52 +0700

    1075. Project Employees I

--- sql/average-selling-price.sql
+++ sql/average-selling-price.sql
@@ -1,12 +1,12 @@
-SELECT
+select
     p.product_id,
     IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
-FROM
-    Prices AS p
-LEFT JOIN
-    UnitsSold AS u
-ON
+from
+    prices AS p
+left join
+    unitssold AS u
+on
     p.product_id = u.product_id
-    AND u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY
+    and u.purchase_date between p.start_date and p.end_date
+group by
     p.product_id;
