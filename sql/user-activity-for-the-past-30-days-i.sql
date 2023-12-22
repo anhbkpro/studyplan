7b3ab9cd66a9d75f490ab4dbdd2798e546a28d36
Anh Lai <laingocanh1990@gmail.com>
2023-12-22 16:19:41 +0700

    1141. User Activity for the Past 30 Days I

---
+++ sql/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,5 @@
+select activity_date as day, COUNT(DISTINCT user_id) as active_users
+from activity
+where DATEDIFF('2019-07-27', activity_date) < 30
+and DATEDIFF('2019-07-27', activity_date) >= 0
+group by 1
