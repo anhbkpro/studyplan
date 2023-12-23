f5bd5a3c3e662449fcda647cc83908091314bf9e
Anh Lai <laingocanh1990@gmail.com>
2023-12-23 13:40:17 +0700

    1731. The Number of Employees Which Report to Each Employee

--- sql/the-number-of-employees-which-report-to-each-employee.sql
+++ sql/the-number-of-employees-which-report-to-each-employee.sql
@@ -12,3 +12,15 @@ from employees e1
 where reports_to is not null
 group by 1
 order by 1
+
+--- Solution 2: faster
+select
+    e1.employee_id,
+    e1.name,
+    COUNT(e2.employee_id) as reports_count,
+    ROUND(AVG(e2.age)) as average_age
+from employees e1
+inner join employees e2
+on e1.employee_id = e2.reports_to
+group by e1.employee_id
+order by e1.employee_id
