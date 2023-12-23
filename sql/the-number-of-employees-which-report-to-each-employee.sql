a6b5f07edf55a05ad6704b27f18956f9b0acfdb1
Anh Lai <laingocanh1990@gmail.com>
2023-12-23 13:30:15 +0700

    1731. The Number of Employees Which Report to Each Employee

---
+++ sql/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,13 @@
+select
+  reports_to as employee_id,
+  (
+    select e2.name
+    from employees e2
+    where e1.reports_to = e2.employee_id
+    limit 1) as name,
+    COUNT(employee_id) as reports_count,
+    ROUND(SUM(age)/COUNT(employee_id), 0) as average_age
+from employees e1
+where reports_to is not null
+group by 1
+order by 1
