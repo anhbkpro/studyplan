c25858953077604c4da4638775330087778bad69
Anh Lai <laingocanh1990@gmail.com>
2023-12-23 14:05:45 +0700

    add comment to explain each query purpose

--- sql/primary-department-for-each-employee.sql
+++ sql/primary-department-for-each-employee.sql
@@ -1,10 +1,12 @@
 -- Approach 1: UNION
+-- Retrieving employees that appear exactly once in the Employee
 with exact_once as (
     select employee_id, department_id
     from employee
     group by 1
     having COUNT(employee_id) = 1
 ),
+-- Retrieving employees with primary_flag set to 'Y'
 prim_emp as (
     select employee_id, department_id
     from employee
@@ -16,3 +18,4 @@ from prim_emp
 union
 select employee_id, department_id
 from exact_once
+
