5a0bcc1513d56a41f7c27fb92f540bab105c45c1
Anh Lai <laingocanh1990@gmail.com>
2023-12-23 10:53:57 +0700

    619. Biggest Single Number

--- sql/biggest-single-number.sql
+++ sql/biggest-single-number.sql
@@ -4,3 +4,22 @@ from
 from mynumbers
 group by 1) as g
 where g.count = 1
+
+-- using HAVING
+with f as (
+    select num
+    from mynumbers
+    group by 1
+    having COUNT(num) = 1
+)
+select MAX(f.num) as num
+from f
+
+-- using window function
+with f as (
+    select num, COUNT(num) over (partition by num) as count
+    from mynumbers
+)
+select MAX(f.num) as num
+from f
+where f.count = 1
