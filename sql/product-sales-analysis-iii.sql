be482478d445ace1f639efedc42df025b671134e
Anh Lai <laingocanh1990@gmail.com>
2023-12-23 10:13:34 +0700

    1070. Product Sales Analysis III

---
+++ sql/product-sales-analysis-iii.sql
@@ -0,0 +1,6 @@
+-- product is redundant
+select product_id, year as first_year, quantity, price
+from sales
+where (product_id, year) in (select product_id, MIN(year) as year
+from sales
+group by 1)
