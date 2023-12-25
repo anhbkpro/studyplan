5f462d4247fa2a0b8b79f1e5d6d792c6e7ff603a
Anh Lai <laingocanh1990@gmail.com>
2023-12-25 15:18:14 +0700

    1907. Count Salary Categories

--- sql/count-salary-categories.sql
+++ sql/count-salary-categories.sql
@@ -16,9 +16,31 @@ high as (
 )
 select "Low Salary" as category, cnt as accounts_count
 from low
-UNION
+union
 select "Average Salary" as category, cnt as accounts_count
 from average
-UNION
+union
 select "High Salary" as category, cnt as accounts_count
 from high
+
+--- Solution 2
+select
+    'Low Salary' as category,
+    SUM(case when income < 20000 then 1 else 0 end) as accounts_count
+from
+    accounts
+
+union
+select
+    'Average Salary' category,
+    SUM(case when income >= 20000 AND income <= 50000 then 1 else 0 end)
+    as accounts_count
+from
+    accounts
+
+union
+select
+    'High Salary' category,
+    SUM(case when income > 50000 then 1 else 0 end) as accounts_count
+from
+    accounts
