3eba3adbfe7d21dd5f3e687f25bb42554bbda122
Anh Lai <laingocanh1990@gmail.com>
2024-01-04 08:18:24 +0700

    585. Investments in 2016

---
+++ sql/investments-in-2016.sql
@@ -0,0 +1,18 @@
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance i
+JOIN
+   (
+   SELECT tiv_2015
+   FROM Insurance
+   GROUP BY tiv_2015
+   HAVING COUNT(DISTINCT pid) > 1
+   ) AS t0
+ON i.tiv_2015 = t0.tiv_2015
+JOIN
+   (
+   SELECT CONCAT(lat, lon) AS lat_lon
+   FROM Insurance
+   GROUP BY CONCAT(lat, lon)
+   HAVING COUNT(DISTINCT pid) = 1
+   ) AS t1
+ON CONCAT(i.lat, i.lon) = t1.lat_lon
