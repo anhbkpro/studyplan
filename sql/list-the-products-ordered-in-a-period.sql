2ce21b59b3176d07524ab0ce0bba7f4d6971121d
Anh Lai <laingocanh1990@gmail.com>
2024-01-07 16:07:10 +0700

    1327. List the Products Ordered in a Period

--- sql/list-the-products-ordered-in-a-period.sql
+++ sql/list-the-products-ordered-in-a-period.sql
@@ -2,6 +2,6 @@ select p.product_name, sum(o.unit) as unit
 from products p
 join orders o
 on p.product_id = o.product_id
-where o.order_date >= '2020-02-01' and o.order_date <'2020-03-01'
+where o.order_date >= '2020-02-01' and o.order_date < '2020-03-01'
 group by 1
 having unit >= 100
