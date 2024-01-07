68232ba73f3685820ad9908872f1ce98e16acbca
Anh Lai <laingocanh1990@gmail.com>
2024-01-07 17:02:11 +0700

    1174. Immediate Food Delivery II

---
+++ sql/immediate-food-delivery-ii.sql
@@ -0,0 +1,10 @@
+select ROUND(sum(
+    case
+        when r.order_date = r.customer_pref_delivery_date then 1
+        else 0
+    end
+) / count(*) * 100, 2) as immediate_percentage from
+(select *,
+DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY order_date) as rnk
+from delivery) r
+where r.rnk = 1
