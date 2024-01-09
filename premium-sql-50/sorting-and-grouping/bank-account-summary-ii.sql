aee02ac4502f6b51e0947eb63707b0ee6a3f4cbb
Anh Lai <laingocanh1990@gmail.com>
2024-01-09 09:44:42 +0700

    refactor to make SQL simplier

--- premium-sql-50/sorting-and-grouping/bank-account-summary-ii.sql
+++ premium-sql-50/sorting-and-grouping/bank-account-summary-ii.sql
@@ -8,3 +8,11 @@ from
     group by 1
 ) g
 where balance > 10000
+
+-- refactor: use having instead of where
+select name, SUM(amount) as balance
+from users
+left join transactions
+using (account)
+group by 1
+having SUM(amount) > 10000
