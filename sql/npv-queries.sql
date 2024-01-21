9465e8ad09d6763952858c69e4ec138c3103616b
Anh Lai <laingocanh1990@gmail.com>
2024-01-21 17:07:40 +0700

    1421. NPV Queries

--- sql/npv-queries.sql
+++ sql/npv-queries.sql
@@ -1,15 +1,5 @@
-with years as
-(
-    select year from npv
-)
 select q.id, q.year,
-    IFNULL(
-        CASE
-            WHEN q.year in (select * from years) then n.npv
-            ELSE 0
-        END,
-        0
-    ) as npv
+    IFNULL(n.npv, 0) AS npv -- or COALESCE(n.npv, 0) AS npv
 from queries q
 left outer join npv n
 using (id, year)
