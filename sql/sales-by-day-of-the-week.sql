e8585957963090211f05cc97f16e524ad7894c42
Anh Lai <laingocanh1990@gmail.com>
2024-02-20 09:07:19 +0700

    1479. Sales by Day of the Week

--- sql/sales-by-day-of-the-week.sql
+++ sql/sales-by-day-of-the-week.sql
@@ -1,34 +1,12 @@
-with DaysOfWeekOrders as(
-    select item_category as CATEGORY,
-    IFNULL(CASE WHEN day_of_week = 2 THEN SUM(quantity) END, 0) as MONDAY,
-    IFNULL(CASE WHEN day_of_week = 3 THEN SUM(quantity) END, 0) as TUESDAY,
-    IFNULL(CASE WHEN day_of_week = 4 THEN SUM(quantity) END, 0) as WEDNESDAY,
-    IFNULL(CASE WHEN day_of_week = 5 THEN SUM(quantity) END, 0) as THURSDAY,
-    IFNULL(CASE WHEN day_of_week = 6 THEN SUM(quantity) END, 0) as FRIDAY,
-    IFNULL(CASE WHEN day_of_week = 7 THEN SUM(quantity) END, 0) as SATURDAY,
-    IFNULL(CASE WHEN day_of_week = 1 THEN SUM(quantity) END, 0) as SUNDAY
-    from
-    (
-        select order_id, customer_id, order_date, o.item_id, quantity, i.item_category,
-            -- 1 = Sunday
-            -- 2 = Monday
-            -- ...
-            -- 7 = Satuday
-            dayofweek(order_date) as day_of_week
-        from orders o
-        right join items i on i.item_id = o.item_id
-    ) j
-    group by item_category, day_of_week
-    order by item_category
-)
-
-select CATEGORY,
-    MAX(MONDAY) as MONDAY,
-    MAX(TUESDAY) as TUESDAY,
-    MAX(WEDNESDAY) as WEDNESDAY,
-    MAX(THURSDAY) as THURSDAY,
-    MAX(FRIDAY) as FRIDAY,
-    MAX(SATURDAY) as SATURDAY,
-    MAX(SUNDAY) as SUNDAY
-from DaysOfWeekOrders
-group by 1
+select item_category as CATEGORY,
+    IFNULL(SUM(CASE WHEN dayofweek(order_date) = 2 THEN quantity END), 0) as MONDAY,
+    IFNULL(SUM(CASE WHEN dayofweek(order_date) = 3 THEN quantity END), 0) as TUESDAY,
+    IFNULL(SUM(CASE WHEN dayofweek(order_date) = 4 THEN quantity END), 0) as WEDNESDAY,
+    IFNULL(SUM(CASE WHEN dayofweek(order_date) = 5 THEN quantity END), 0) as THURSDAY,
+    IFNULL(SUM(CASE WHEN dayofweek(order_date) = 6 THEN quantity END), 0) as FRIDAY,
+    IFNULL(SUM(CASE WHEN dayofweek(order_date) = 7 THEN quantity END), 0) as SATURDAY,
+    IFNULL(SUM(CASE WHEN dayofweek(order_date) = 1 THEN quantity END), 0) as SUNDAY
+from items i
+left join orders o on o.item_id = i.item_id
+group by item_category
+order by item_category
